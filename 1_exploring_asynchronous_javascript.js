e96d602f3d4f7ba8c77e5aceea9a7c975c7a9f04
BGreenberg79 <greenbeb2@gmail.com>
2024-10-12 16:52:13 -0400

    2nd commit

--- 1_exploring_asynchronous_javascript.js
+++ 1_exploring_asynchronous_javascript.js
@@ -16,7 +16,7 @@ let publicKey ="d439bcb5d546b0de1cbff183b2365de5"
 let privateKey = "b4f967f271fbc92b00eed71fe6180d2c5fad88fd"
 let hashVal ="3fe5c988438c73b5ca517fe98be46e24"
 
-async function fetchCharacter(callback){
+function fetchCharacter(callback){
     button.addEventListener("click", (getResult = async() => {
         if (input.value.trim().length < 1){
             alert("Input cannot be empty");
@@ -31,16 +31,18 @@ async function fetchCharacter(callback){
         catch (error) {
             callback(error, null);
         };
+        console.log(characterData);
 }))}
 
-const feedToHTML = (error, data) => {
+const feedToHTML = (error, characterData) => {
     const dataInfoId = document.getElementById('display-container');
 
     if (error) {
         dataInfoId.innerHTML = `<p>Error Fetching Character Data: ${error.message}</p>`;
-    } else {
-        dataInfoId.innerHTML = `<p>Character Data: ${data.message}</p>`;
-    }
+    } 
+    else {
+        dataInfoId.innerHTML = `<p>Character Data: ${characterData.message}</p>`;
+    };
 };
 
 const updatePage = () => {
